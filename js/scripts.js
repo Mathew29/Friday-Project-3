38f5a4caa4c3f03ab70b038a260332ae4e140303
Mathew Akre <mwakre29@gmail.com>
2019-04-05 14:26:56 -0700

    add html

--- js/scripts.js
+++ js/scripts.js
@@ -31,39 +31,11 @@ function inputLoop(number) {
   console.log(number);
   return numberArray;
 }
-// var number = 12
-// console.log(inputLoop(number));
-// function inputCheck(number) {
-//   if(isNaN(number)) {
-//     alert("Not a number")
-//   } else if (number < 0){
-//     alert("please enter a positive number")
-//   }
-//   return number;
-// }
-// function inputLoop(number) {
-//   var numberArray = [];
-//   var beep = "Beep!";
-//   var boop = "Boop!";
-//   var sorryDave = "I'm sorry, Dave. I'm afraid I can't do that.";
-//   for (var i = 0; i <= number; i++) {
-//     numberString = number.toString();
-//     if (numberString.includes("3")) {
-//       numberArray.push(sorryDave);
-//     } else if (numberString.includes("2")) {
-//       numberArray.push(boop);
-//     }else if (numberString.includes("1")) {
-//       numberArray.push(beep);
-//     } else {
-//       numberArray.push(i.toString());
-//     }
-//   }
-//   return numberArray;
-// }
-//
-// var number = 1
-// console.log(inputLoop(number));
-// console.log(inputCheck(number));
+
+
+
+
+
 // User Interface
 $(document).ready(function() {
   $("#user").submit(function(event) {
